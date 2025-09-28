cd4cd6c5376003736cd7d9fb97424ccb752dd908
IDMOP0 <marcos.oriol@idneo.com>
2025-09-28 18:20:04 +0200

    fix: put current year instead of fixed yar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import './Footer.css';
 
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer-content">
-          <p>&copy; 2024 Carmen Sierra Sancho. All rights reserved.</p>
+          <p>&copy; {currentYear} Carmen Sierra Sancho. All rights reserved.</p>
           <p>Architecture Portfolio</p>
         </div>
       </div>
